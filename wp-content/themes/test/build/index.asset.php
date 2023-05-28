fb0e60493a2f3944c7650a872660675deee47bf3
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-05-28 15:08:22 +0300

    test-work

--- wp-content/themes/test/build/index.asset.php
+++ wp-content/themes/test/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'd4a4f30c188eaa567f87');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '8f393729241ea9de8dbe');
